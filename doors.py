d3a9786ae9480a279f560a1130b1ea1e6f0e4459
Sicilat <sicilat@gmail.com>
2019-04-04 22:09:34 +0200

    100 doors finished + micro modification to main

--- doors.py
+++ doors.py
@@ -1,5 +1,34 @@
 def run():
-	print('test')
+	doors_open = []
+	doors_close = []
+	doors_list = [1 for i in range(100)]
+	i = 1
+	while i < len(doors_list):
+		if doors_list[i] == 0:
+			doors_list[i] = 1
+		else:
+			doors_list[i] = 0
+		i += 2
+	i = 2
+	while i < len(doors_list):
+		if doors_list[i] == 0:
+			doors_list[i] = 1
+		else:
+			doors_list[i] = 0
+		i += 3
+	i = 0
+	while i < len(doors_list):
+		if doors_list[i] == 0:
+			doors_open.append(i + 1)
+		else:
+			doors_close.append(i + 1)
+		i += 1
+	print('Doors open : \n')
+	for p in doors_open:
+		print(p)
+	print('\nDoors closed : \n')
+	for p in doors_close:
+		print(p)
 
 class main(object):
 	def __init__(self):
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def menu(codes_list):
 	clear()
 	i = 0
 	for p in codes_list:
-		i = i + 1
+		i += 1
 		print(str(i) + ' - ' + p)
 	c_id = int(input('> '))
 	while c_id <= 0 or c_id >= len(codes_list) + 1:
